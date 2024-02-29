dde2e91f565083649727430d2ef192692302dfa8
DashaPoliazz <dasha.poliazzz@gmail.com>
2024-02-29 18:04:22 +0200

    wasm-memory

--- www/index.js
+++ www/index.js
@@ -1,14 +1,44 @@
 (async () => {
+  // creating memory with JS:
+  const memory = new WebAssembly.Memory({
+    initial: 1,
+  });
+
   const importObject = {
+    js: {
+      memory,
+    },
     console: {
       log: () => console.log("log"),
       error: () => console.error("error"),
     },
   };
 
-  const wasmFile = await fetch("test.wasm");
+  const wasmFile = await fetch("js-to-wasm-memory.wasm");
   const buffer = await wasmFile.arrayBuffer();
   const wasm = await WebAssembly.instantiate(buffer, importObject);
-  const { addTwo } = wasm.instance.exports;
-  addTwo(2, 2);
+
+  // 1 page -> 65Kb
+  const { wasmMemory } = wasm.instance.exports;
+
+  const uint8 = new Uint8Array(wasmMemory.buffer);
+  const decoder = new TextDecoder();
+  // reading first 2 bytes
+  const chunkToDecode = uint8.slice(0, 2);
+  const result = decoder.decode(chunkToDecode);
+  // js-to-wasm-memory
+  const foo = "foo";
+  const u8Arr = new Uint8Array(memory.buffer);
+  for (let i = 0; i < foo.length; i++) {
+    const charCode = foo[i].charCodeAt();
+    u8Arr[i] = charCode;
+  }
+
+  let decoded = "";
+  for (let i = 0; i < foo.length; i++) {
+    const ascii = u8Arr[i];
+    const decodedChar = String.fromCharCode(ascii);
+    decoded = decoded.concat(decodedChar);
+  }
+  console.log(decoded);
 })();
